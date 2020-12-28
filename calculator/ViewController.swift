12417b77ba537360a0aa0f08e1bf65ab9f8e93c3
Erdem Özkök <51801996+erdemozk@users.noreply.github.com>
2020-12-28 19:13:27 +0300

    First Swift Calculator Project

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstNum: UITextField!
+    @IBOutlet weak var secondNum: UITextField!
+    @IBOutlet weak var resultText: UILabel!
+    
+    var result = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sumButton(_ sender: Any) {
+        if let firstNumber = Int(firstNum.text!) {
+            if let secondNumber = Int(secondNum.text!) {
+             
+                result = firstNumber + secondNumber
+                resultText.text = String(result)
+                
+            }
+        }
+    }
+    
+    @IBAction func exractionButton(_ sender: Any) {
+        if let firstNumber = Int(firstNum.text!) {
+            if let secondNumber = Int(secondNum.text!) {
+                
+                result = firstNumber - secondNumber
+                resultText.text = String(result)
+                
+            }
+        }
+    }
+    
+    @IBAction func divideButton(_ sender: Any) {
+        if let firstNumber = Int(firstNum.text!) {
+            if let secondNumber = Int(secondNum.text!) {
+                
+                result = firstNumber / secondNumber
+                resultText.text = String(result)
+                
+            }
+        }
+    }
+    
+    @IBAction func multiplyButton(_ sender: Any) {
+        if let firstNumber = Int(firstNum.text!) {
+            if let secondNumber = Int(secondNum.text!) {
+                
+                result = firstNumber * secondNumber
+                resultText.text = String(result)
+                
+            }
+        }
+    }
+    
+    
 
 }
 
